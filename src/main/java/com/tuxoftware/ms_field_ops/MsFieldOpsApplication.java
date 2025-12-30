be36a7891e14836247cf5ee7a25b526ed51709c2
Hugo Cobos <hcobos@azurian.com>
2025-12-29 18:40:25 -0600

    first commit

---
+++ src/main/java/com/tuxoftware/ms_field_ops/MsFieldOpsApplication.java
@@ -0,0 +1,13 @@
+package com.tuxoftware.ms_field_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFieldOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFieldOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuxoftware/ms_field_ops/MsFieldOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuxoftware.ms_field_ops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFieldOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
